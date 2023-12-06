3f8fd2bbe8d149d12e467127d7d0302bc2893991
brainbuzz1 <rajsumit22032003@gmail.com>
2023-12-06 17:21:47 +0530

    deploment ready

--- woolee_backend/settings.py
+++ woolee_backend/settings.py
@@ -130,4 +130,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ['https://woolee-backend-riosumit.vercel.app']
+CORS_ORIGIN_ALLOW_ALL = True
